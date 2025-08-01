eb65b49720018241a885cca40c5328df5ee71d15
namkang <namkang.mbw@gmail.com>
2025-08-01 22:01:15 +0700

    assign host

--- app.py
+++ app.py
@@ -776,7 +776,8 @@ def report_pdf():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 when run locally
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 
 
